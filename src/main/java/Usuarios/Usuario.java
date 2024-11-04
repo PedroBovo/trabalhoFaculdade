8d4405b21caac1a420e6307fc6eccb7d1dddb99f
pedro bovo <pedrobovo123@gmail.com>
2024-11-04 00:03:38 -0300

    Começando trabalho de Interface

---
+++ src/main/java/Usuarios/Usuario.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Usuarios;
+
+/**
+ *
+ * @author Pedro
+ */
+public class Usuario {
+    
+    private String Nome;
+    private int idade;
+    private String cpf;
+    private String senha;
+    
+    public Usuario(){}
+
+    public Usuario(String Nome, int idade, String cpf, String senha) {
+        this.Nome = Nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+    
+    
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    
+    
+    
+}
